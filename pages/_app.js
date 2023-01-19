913e98beacd1bab8119aab7858db0b9f55a9f505
olliekm <revilokm@gmail.com>
2023-01-19 09:50:01 -0500

    fixing comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+/**
+ * Author: Oliver Kwun-Morfitt
+ * Date: 2023/01/19
+ * Description: An online programming forum for high school
+ * students who want to share about their coding journeys
+ * 
+ */
+
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
